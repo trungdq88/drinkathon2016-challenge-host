d6626fb1288a4920e3e6f82950b9ffe83fe69576
Trung Dinh Quang <trungdq88@gmail.com>
2016-03-16 12:03:45 +0700

    Level 9 (no GPS yet)

---
+++ levels/level-9/api.js
@@ -0,0 +1,66 @@
+var express = require('/node_modules/express');
+var bodyParser = require('/node_modules/body-parser');
+var fs = require('fs');
+
+var level10 = require('/secret/levels/10.js');
+var level9 = require('/secret/levels/9.js');
+var PORT = 7779;
+
+
+// Error reporting
+process.on('uncaughtException', function(err) {
+  console.log(err);
+});
+
+// Start express server
+var server = express();
+
+// Serve static files at root url (React/Redux client)
+server.use('/', express.static(__dirname));
+server.use(bodyParser.json());       // to support JSON-encoded bodies
+server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true,
+}));
+/**
+ * Set up API server
+ */
+var startApi = function () {
+
+  var router = express.Router();
+
+  router.get('/hello', function (req, res) {
+    res.json({hello: 'world'});
+  });
+
+  router.post('/next', function (req, res) {
+    if (level10.check(req.body.island_name,
+                      req.body.island_lat,
+                      req.body.island_lng)) {
+      res.json({next_island_url: 'http://asd'});
+    } else {
+      res.json({error: 'Hm... where is that?'});
+    }
+  });
+
+  router.post('/show', function (req, res) {
+    if (req.body.island_name === level9.name &&
+        req.body.island_lat === level9.lat &&
+        req.body.island_lng === level9.lng) {
+      res.sendFile(__dirname + '/question.html');
+    } else {
+      res.json({error: 'Uhh... we only accept guests from previous island.'});
+    }
+  });
+
+  // API routes
+  server.use('/api', router);
+
+  // Start server
+  server.listen(PORT, function () {
+    console.log('Express server listening on port ' + PORT);
+  });
+
+};
+
+startApi();
+
